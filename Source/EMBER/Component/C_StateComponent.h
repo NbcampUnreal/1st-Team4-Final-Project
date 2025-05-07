a70795322afbc25a6cfd8dd38f2179bd323737c6
Ektoba <wypka78@gmail.com>
2025-05-07 21:14:16 +0900

    Struct와 Flag추가
    [✨ update ] FIGMA에 있는 Struct Enum모두 추가

---
+++ Source/EMBER/Component/C_StateComponent.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "C_StateComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class EMBER_API UC_StateComponent : public UActorComponent
+{
+	GENERATED_BODY()
+public:	
+	UC_StateComponent();
+protected:
+	virtual void BeginPlay() override;
+public:	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
