067ea6335f79647d4bb15cb61e27ae82fc9b522e
Big_Baek <euihyun1188@gmail.com>
2025-05-07 21:04:18 +0900

    [feat] Add Basic Boar
    -Boar 테스트중

---
+++ Source/EMBER/AI/C_Boar.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/C_Boar.h"
+
+AC_Boar::AC_Boar()
+{
+	MaxHP = 100;
+	CurrentHP = MaxHP;
+	Speed = 50;
+	bIsDie = false;
+}
---
+++ Source/EMBER/AI/C_Boar.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/BaseAI.h"
+#include "C_Boar.generated.h"
+
+
+UCLASS()
+class EMBER_API AC_Boar : public ABaseAI
+{
+	GENERATED_BODY()
+
+public:
+	AC_Boar();
+	
+	
+};
