bd85415ddb45fccaaf2af9c04e02f86ab2be42ce
Big_Baek <euihyun1188@gmail.com>
2025-05-08 13:03:10 +0900

    [feature] boar rename

---
+++ Source/EMBER/AI/Animal/Boar.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Animal/Boar.h"
+
+ABoar::ABoar()
+{
+	MaxHP = 100;
+	CurrentHP = MaxHP;
+	Speed = 50;
+	bIsDie = false;
+}
---
+++ Source/EMBER/AI/Animal/Boar.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/BaseAI.h"
+#include "Boar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMBER_API ABoar : public ABaseAI
+{
+	GENERATED_BODY()
+
+public:
+	ABoar();
+
+};
