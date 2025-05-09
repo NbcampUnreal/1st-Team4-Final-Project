f34169d2eb0f79ebae82e045c080c4c3ac66c12d
DevKolIn <donghyeon.study@gmail.com>
2025-05-09 13:09:50 +0900

    [feat] CheatManager에 필요한 데이터 저장 및 참조 구현 #50

--- Source/EMBER/System/EmberAssetManager.h
+++ Source/EMBER/System/EmberAssetManager.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "Engine/AssetManager.h"
+#include "UI/Data/EmberAssetData.h"
 #include "UI/Data/EmberItemData.h"
 #include "UI/Data/EmberUIData.h"
 
@@ -26,9 +27,16 @@ protected:
 
 public:
 	static UEmberAssetManager& Get();
+
+	template<typename AssetType>
+	static TSubclassOf<AssetType> GetSubclassByPath(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+	
+	template<typename AssetType>
+	static TSubclassOf<AssetType> GetSubclassByName(const FName& AssetName, bool bKeepInMemory = true);
 	
 	const UEmberUIData& GetUIData();
 	const UEmberItemData& GetItemData();
+	const UEmberAssetData& GetAssetData();
 
 protected:
 	UPrimaryDataAsset* LoadGameDataOfClass(TSubclassOf<UPrimaryDataAsset> DataClass, const TSoftObjectPtr<UPrimaryDataAsset>& DataClassPath, FPrimaryAssetType PrimaryAssetType);
@@ -43,6 +51,11 @@ protected:
 		
 		return *CastChecked<const GameDataClass>(LoadGameDataOfClass(GameDataClass::StaticClass(), DataPath, GameDataClass::StaticClass()->GetFName()));
 	}
+
+	static UObject* SynchronousLoadAsset(const FSoftObjectPath& AssetPath);
+
+	// Thread safe way of adding a loaded asset to keep in memory.
+	void AddLoadedAsset(const UObject* Asset);
 	
 protected:
 	UPROPERTY(Config)
@@ -51,6 +64,55 @@ protected:
 	UPROPERTY(Config)
 	TSoftObjectPtr<UEmberItemData> ItemDataPath;
 
+	UPROPERTY(Config)
+	TSoftObjectPtr<UEmberAssetData> AssetDataPath;
+	
 	UPROPERTY(Transient)
 	TMap<TObjectPtr<UClass>, TObjectPtr<UPrimaryDataAsset>> GameDataMap;
+
+private:
+	// Assets loaded and tracked by the asset manager.
+	UPROPERTY()
+	TSet<TObjectPtr<const UObject>> LoadedAssets;
+	
+	// Used for a scope lock when modifying the list of load assets.
+	FCriticalSection LoadedAssetsCritical;
 };
+
+template <typename AssetType>
+TSubclassOf<AssetType> UEmberAssetManager::GetSubclassByPath(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	TSubclassOf<AssetType> LoadedSubclass;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		LoadedSubclass = AssetPointer.Get();
+		if (!LoadedSubclass)
+		{
+			LoadedSubclass = Cast<UClass>(SynchronousLoadAsset(AssetPath));
+		}
+
+		if (LoadedSubclass && bKeepInMemory)
+		{
+			Get().AddLoadedAsset(Cast<UObject>(LoadedSubclass));
+		}
+	}
+
+	return LoadedSubclass;
+}
+
+template <typename AssetType>
+TSubclassOf<AssetType> UEmberAssetManager::GetSubclassByName(const FName& AssetName, bool bKeepInMemory)
+{
+	const UEmberAssetData& AssetData = Get().GetAssetData();
+	const FSoftObjectPath& AssetPath = AssetData.GetAssetPathByName(AssetName);
+	
+	FString AssetPathString = AssetPath.GetAssetPathString();
+	AssetPathString.Append(TEXT("_C"));
+
+	FSoftClassPath ClassPath(AssetPathString);
+	TSoftClassPtr<AssetType> ClassPtr(ClassPath);
+	return GetSubclassByPath<AssetType>(ClassPtr, bKeepInMemory);
+}
