d1e17057cc35229e027dc020454579cbc3d75ee9
Big_Baek <euihyun1188@gmail.com>
2025-05-09 20:11:19 +0900

    [Update] AIBlackboard 업데이트

--- Source/EMBER/AI/BTT_Guard.cpp
+++ Source/EMBER/AI/BTT_Guard.cpp
@@ -7,3 +7,10 @@ UBTT_Guard::UBTT_Guard()
 {
 	NodeName = "GuardState";
 }
+
+EBTNodeResult::Type UBTT_Guard::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	
+
+	return EBTNodeResult::Succeeded;
+}
