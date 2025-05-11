651a4a645834d009497fb3c1687a06bfe8d626ec
DevKolIn <donghyeon.study@gmail.com>
2025-05-11 20:00:51 +0900

    [add] 인벤토리 아이템 수량 표시용 위젯 구현 #59
    - 필요한 애셋 추가
    - 아이템 수량 변경시 UI 업데이트 기능 구현

---
+++ Source/EMBER/UI/Inventory/InventoryEntryWidget.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ItemWidgets/ItemEntryWidget.h"
+#include "InventoryEntryWidget.generated.h"
+
+class USizeBox;
+class UItemInstance;
+class UInventorySlotsWidget;
+
+UCLASS()
+class EMBER_API UInventoryEntryWidget : public UItemEntryWidget
+{
+	GENERATED_BODY()
+
+public:
+	UInventoryEntryWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+public:
+	void Init(UInventorySlotsWidget* InSlotsWidget, UItemInstance* InItemInstance, int32 InItemCount);
+
+private:
+	UPROPERTY()
+	TObjectPtr<UInventorySlotsWidget> SlotsWidget;
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<USizeBox> SizeBox_Root;
+};
