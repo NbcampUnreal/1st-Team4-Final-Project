6e76ed700db8b7002a6595e779bf91b1902f7056
Big_Baek <euihyun1188@gmail.com>
2025-05-12 19:49:57 +0900

    [feat] PassiveAI
    - 토끼추가
    - GuardTask
    플레이어 감지시 정지후 플레이어를 바라봄, 속도값을 증가시킴
    -RunTask
    플레이어가 가까워질 시 반대방향으로 도망
    -PassiveAI
    1초마다 가장 가까운적을 갱신하여 대상에맞게 Task 실행
    
    이후 수정사항
    1. 가까운적을 항상 갱신하는게 아닌 Guard, Run Task실행중일 때만 타이머를 실행
    2. 감지대상의 타입이 Player일떄만 Task를 실행하도록 설정

--- Source/EMBER/AI/Task/BTT_Run.cpp
+++ Source/EMBER/AI/Task/BTT_Run.cpp
@@ -3,7 +3,33 @@
 
 #include "BTT_Run.h"
 
+#include "BaseAI.h"
+#include "BaseAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
 UBTT_Run::UBTT_Run()
 {
 	NodeName = "RunState";
+}
+
+EBTNodeResult::Type UBTT_Run::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	ABaseAIController* Controller = Cast<ABaseAIController>(OwnerComp.GetOwner());
+	UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent();
+
+	AActor* Target = Cast<AActor>(BlackboardComponent->GetValueAsObject("TargetActor"));
+	if (Target==nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+	ABaseAI* ControlledAnimal = Cast<ABaseAI>(Controller->GetPawn());
+	
+	FVector TargetLocation = Target->GetActorLocation();
+	FVector AI_Location = ControlledAnimal->GetActorLocation();
+
+	FVector Direction = (AI_Location - TargetLocation).GetSafeNormal();//방향벡터만 남기고 1로 설정
+	FVector NewLocation = AI_Location + Direction * 2000.0f;
+	Controller->MoveToLocation(NewLocation);
+
+	return EBTNodeResult::Succeeded;
 }
