b8156a0d624e107876ecbdcdd4bdabf4ca1dba72
DevKolIn <donghyeon.study@gmail.com>
2025-05-13 10:47:13 +0900

    [update] 치트 매니저 및 인벤토리 업데이트 #75

---
+++ Source/EMBER/System/AbilitySystem/Data/EmberAbilitySet.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "EmberAbilitySet.generated.h"
+
+struct FGameplayAbilitySpecHandle;
+class UEmberAbilitySystemComponent;
+class UEmberGameplayAbility;
+class UAttributeSet;
+
+USTRUCT(BlueprintType)
+struct FEmberAbilitySet_GameplayAbility
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UEmberGameplayAbility> Ability;
+	
+	UPROPERTY(EditDefaultsOnly)
+	int32 AbilityLevel = 1;
+	
+	UPROPERTY(EditDefaultsOnly, Meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+};
+
+USTRUCT(BlueprintType)
+struct FEmberAbilitySet_GrantedHandles
+{
+	GENERATED_BODY()
+
+public:
+	void AddAbilitySpecHandle(const FGameplayAbilitySpecHandle& Handle);
+	void TakeFromAbilitySystem(UEmberAbilitySystemComponent* EmberASC);
+
+protected:
+	UPROPERTY()
+	TArray<FGameplayAbilitySpecHandle> AbilitySpecHandles;
+	
+	UPROPERTY()
+	TArray<TObjectPtr<UAttributeSet>> GrantedAttributeSets;
+};
+
+UCLASS(BlueprintType, Const)
+class EMBER_API UEmberAbilitySet : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UEmberAbilitySet(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+public:
+	void GiveToAbilitySystem(UEmberAbilitySystemComponent* LyraASC, FEmberAbilitySet_GrantedHandles* OutGrantedHandles, UObject* SourceObject = nullptr) const;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay Abilities")
+	TArray<FEmberAbilitySet_GameplayAbility> GrantedGameplayAbilities;
+};
---
+++ Source/EMBER/UI/Data/EmberPawnData.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "EmberPawnData.generated.h"
+
+class UEmberAbilitySet;
+class UEmberInputConfig;
+/**
+ * 
+ */
+UCLASS(BlueprintType, Const)
+class EMBER_API UEmberPawnData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UEmberPawnData(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+public:
+	static const UEmberPawnData& Get();
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ember|Abilities")
+	TArray<TObjectPtr<UEmberAbilitySet>> AbilitySets;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ember|Input")
+	TObjectPtr<UEmberInputConfig> InputConfig;
+};
