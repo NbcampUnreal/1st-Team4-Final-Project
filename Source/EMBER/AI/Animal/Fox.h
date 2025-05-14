b33f80c388bc3964bfa0d698843e603f009eb925
haeuny123 <haeuny123@gmail.com>
2025-05-14 15:32:03 +0900

    여우 작업중

---
+++ Source/EMBER/AI/Animal/Fox.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/DefensiveAI.h"
+#include "Fox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMBER_API AFox : public ADefensiveAI
+{
+	GENERATED_BODY()
+	
+};
