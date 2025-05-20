dd29fa2fc62d01efa946370802e0eb816fc688b4
DevKolIn <donghyeon.study@gmail.com>
2025-05-20 16:18:31 +0900

    [add] 장비창 UI 애셋 추가 #113

---
+++ Source/EMBER/UI/Inventory/Equipment/EquipmentEntryWidget.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UI/Inventory/ItemWidgets/ItemEntryWidget.h"
+#include "EquipmentEntryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMBER_API UEquipmentEntryWidget : public UItemEntryWidget
+{
+	GENERATED_BODY()
+
+public:
+	UEquipmentEntryWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+};
