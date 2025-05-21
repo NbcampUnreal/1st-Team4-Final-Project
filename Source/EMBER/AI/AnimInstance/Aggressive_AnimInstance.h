8b28fa8731f34469592228dc4a68f20c5a84f7a7
Han <gksrudtlr2@icloud.com>
2025-05-21 21:06:10 +0900

    Add Aggressive AI Animation
    Aggressive AI 애니메이션 등 작업

---
+++ Source/EMBER/AI/AnimInstance/Aggressive_AnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/AnimInstance/BaseAIAnimInstance.h"
+#include "Aggressive_AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMBER_API UAggressive_AnimInstance : public UBaseAIAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/EMBER/AI/Animal/Mammoth.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/DefensiveAI.h"
+#include "Mammoth.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMBER_API AMammoth : public ADefensiveAI
+{
+	GENERATED_BODY()
+};
