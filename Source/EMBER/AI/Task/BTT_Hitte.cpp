389622cd7f2a2ad2bca7c3d76fdb5221878246cc
Han <gksrudtlr2@icloud.com>
2025-06-13 19:58:17 +0900

    Update Hit Task

--- Source/EMBER/AI/Task/BTT_Hitte.cpp
+++ Source/EMBER/AI/Task/BTT_Hitte.cpp
@@ -3,3 +3,22 @@
 
 #include "AI/Task/BTT_Hitte.h"
 
+UBTT_Hitte::UBTT_Hitte()
+{
+	NodeName = L"Hitted";
+}
+
+EBTNodeResult::Type UBTT_Hitte::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
+
+EBTNodeResult::Type UBTT_Hitte::AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::AbortTask(OwnerComp, NodeMemory);
+}
+
+void UBTT_Hitte::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+}
