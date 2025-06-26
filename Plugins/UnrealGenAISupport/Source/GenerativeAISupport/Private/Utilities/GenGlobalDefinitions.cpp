d53973284c09dfeb4fc61ec13802c647a4fde901
Ektoba <wypka78@gmail.com>
2025-06-26 16:32:00 +0900

    불필요한거 정리

--- Plugins/UnrealGenAISupport/Source/GenerativeAISupport/Private/Utilities/GenGlobalDefinitions.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Utilities/GenGlobalDefinitions.h"
-
-DEFINE_LOG_CATEGORY(LogGenAI)
-DEFINE_LOG_CATEGORY(LogGenPerformance)
-DEFINE_LOG_CATEGORY(LogGenAIVerbose)
