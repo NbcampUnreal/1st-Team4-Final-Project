3d7bd90f4e6ae367f80ea74b93abb49ebfe84903
DevKolIn <donghyeon.study@gmail.com>
2025-07-21 21:42:14 +0900

    [Remove] 프로젝트 정리

--- Source/EMBER/Item/Fragments/ItemFragment_Equipable_Utility.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "ItemFragment_Equipable_Utility.h"
-
-UItemFragment_Equipable_Utility::UItemFragment_Equipable_Utility(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-	EquipmentType = EEquipmentType::Utility;
-}
