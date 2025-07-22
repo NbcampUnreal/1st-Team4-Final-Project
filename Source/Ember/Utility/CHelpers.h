281d0e1dcd3be5183add7b2f26e2351e269d77a0
Han <gksrudtlr2@icloud.com>
2025-07-23 02:25:41 +0900

    Update Utility
    Update Template

--- Source/Ember/Utility/CHelpers.h
+++ Source/Ember/Utility/CHelpers.h
@@ -26,4 +26,54 @@ public:
 	{
 		*OutComponent = InActor->CreateDefaultSubobject<T>(InName);
 	}
+
+	template<typename T>
+	static void GetAsset(TObjectPtr<T>* OutObject, FString InPath)
+	{
+		ConstructorHelpers::FObjectFinder<T> asset(*InPath);
+		*OutObject = asset.Object;
+	}
+
+	template<typename T>
+	static void GetClass(TSubclassOf<T>* OutClass, FString InPath)
+	{
+		ConstructorHelpers::FClassFinder<T> asset(*InPath);
+		*OutClass = asset.Class;
+	}
+
+	template<typename T>
+	static TObjectPtr<T> FindActor(TObjectPtr<UWorld> InWorld)
+	{
+		for (const TObjectPtr<AActor>& actor : InWorld->GetCurrentLevel()->Actors)
+			if (actor != nullptr && actor.IsA<T>())
+				return Cast<T>(actor);
+
+		return nullptr;
+	}
+
+	template<typename T>
+	static void FindActors(TObjectPtr<UWorld> InWorld, TArray<TObjectPtr<T>>& OutActors)
+	{
+		for (const TObjectPtr<AActor>& actor : InWorld->GetCurrentLevel()->Actors)
+			if (actor != nullptr && actor.IsA<T>())
+				OutActors.Add(Cast<T>(actor));
+	}
+
+	template<typename T>
+	static TObjectPtr<T> GetComponent(TObjectPtr<AActor> InActor)
+	{
+		return Cast<T>(InActor->GetComponentByClass(T::StaticClass()));
+	}
+
+	template<typename T>
+	static TObjectPtr<T> GetComponent(TObjectPtr<AActor> InActor, const FString& InName)
+	{
+		TArray<TObjectPtr<T>> components;
+		InActor->GetComponents<T>(components);
+
+		for (TObjectPtr<T> component : components)
+			if (component.GetName() == InName)
+				return component;
+		return nullptr;
+	}
 };
