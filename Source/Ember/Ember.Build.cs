e3c731eac0cfe6c9a55d1bee6f0ed091e01fa343
haeuny123 <haeuny123@gmail.com>
2025-07-29 02:27:40 +0900

    서버 접속 구현 중

--- Source/Ember/Ember.Build.cs
+++ Source/Ember/Ember.Build.cs
@@ -10,7 +10,7 @@ public class Ember : ModuleRules
 
 		PublicIncludePaths.Add(ModuleDirectory);
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "OnlineSubsystem", "OnlineSubsystemUtils", "OnlineSubsystemSteam" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {"GameFeatures","GameplayAbilities","GameplayTasks","GameplayTags"});
 
